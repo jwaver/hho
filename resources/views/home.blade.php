cbb0804dd741038de612e826e6b4a1b28d96f3ae
jamesjomuad <jamesj@zylun.com>
2015-05-22 10:54:53 +0800

    Looking

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -49,4 +49,13 @@
 				</div>
             </div>
     </div>
+	
+	<div id="socialize" class="row">
+		<div class="col-md-12">
+			<a href="https://www.facebook.com/HHOCebu"><i id="social" class="fa fa-facebook-square fa-3x social-fb"></i></a>
+			<a href="#"><i id="social" class="fa fa-twitter-square fa-3x social-tw"></i></a>
+			<a href="#"><i id="social" class="fa fa-google-plus-square fa-3x social-gp"></i></a>
+			<a href="#"><i id="social" class="fa fa-envelope-square fa-3x social-em"></i></a>
+		</div>
+	</div>
 @endsection
